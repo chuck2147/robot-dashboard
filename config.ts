c5364f002cbaec5c74e6b0243112292a4ec7c0c7
Caleb Eby <caleb.eby01@gmail.com>
2020-02-07 22:47:44 -0800

    Update robot dimensions again

--- config.ts
+++ config.ts
@@ -14,8 +14,8 @@ export const angularDecel = angularAccel
 
 export const curvatureVelocity = 5
 
-export const driveWidth = 21
-export const driveLength = 29
+export const driveWidth = 17.5
+export const driveLength = 26
 
 export const bumperWidth = 29
 export const bumperLength = 38
